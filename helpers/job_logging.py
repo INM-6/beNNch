e60d727a35144f9ac8e0fa28c5336628fbf8c280
jasperalbers <jasperalbers45@gmail.com>
2021-08-17 16:04:01 +0200

    add user and model to metadata, rename base_vp to vps_per_node

--- helpers/job_logging.py
+++ helpers/job_logging.py
@@ -3,21 +3,21 @@ import sys
 import pickle
 
 save_path = sys.argv[1]
-base_vp = sys.argv[2]
-num_vps = sys.argv[3]
-num_nodes = sys.argv[4]
-tasks_per_node = sys.argv[5]
-threads_per_task = sys.argv[6]
-nest = sys.argv[7]
+vps_per_node = sys.argv[2]
+num_nodes = sys.argv[3]
+tasks_per_node = sys.argv[4]
+threads_per_task = sys.argv[5]
+nest = sys.argv[6]
+model_name = sys.argv[7]
 network_state = sys.argv[8]
 
 job_info = {
-    'base_vp': base_vp,
-    'num_vps': num_vps,
+    'vps_per_node': vps_per_node,
     'num_nodes': num_nodes,
     'tasks_per_node': tasks_per_node,
     'threads_per_task': threads_per_task,
     'nest': nest,
+    'model_name': model_name,
     'network_state': network_state,
 }
 
