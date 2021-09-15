803afd98b635f888d8f7cedc2730997dfb76ebe4
jasperalbers <jasperalbers45@gmail.com>
2021-09-15 17:28:18 +0200

    add affinity to metadata

--- analysis/analysis_helper.py
+++ analysis/analysis_helper.py
@@ -35,8 +35,9 @@ def git_annex(cpu_info, job_info, uuidgen_hash, base_path):
 
     for info_dict in [job_info, cpu_info]:
         for key, value in info_dict.items():
+            value_without_spaces = value.replace(' ', ';')
             shell_without_print(f'git annex metadata {result_file_path} '
-                  + f'--set {key}="{value}" --force')
+                  + f'--set {key}="{value_without_spaces}" --force')
     # additionally add machine and user name
     shell_without_print(f'git annex metadata {result_file_path} '
           + f'--set machine="{machine}" --force')
