ca37a0a86d0e9606cf4d36099392c6208bd13d39
jasperalbers <jasperalbers@posteo.de>
2021-11-29 18:25:09 +0100

    Add license information to all files.

--- helpers/cpu_logging.py
+++ helpers/cpu_logging.py
@@ -1,3 +1,21 @@
+"""
+cpu_logging.py: Logs CPU specifications returned by the 'lscpu' shell
+command. This metadata is subsequently annotated to the timing data.
+Copyright (C) <2021> <INM-6>
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+"""
+
 import os
 import sys
 import json
